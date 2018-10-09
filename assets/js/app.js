1a0022cfb96e25d2b1c27158483c5c80474c1f6a
Akim S. Gubanov <akim_now@mail.ru>
2018-10-10 00:27:56 +0300

    Init

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,7 +21,56 @@ $(document).ready(() => {
         $("#loading").removeClass('show');
     });
 
+    /* Hamburger */
     /* jQuery.mmenu */
-    $("#my-menu").mmenu({}, {});
+    $("#my-menu").mmenu({
+        extensions: [
+            "position-front",
+            "position-right",
+            "shadow-page",
+            "fx-menu-slide",
+            "multiline",
+            "pagedim-black",
+            "theme-dark"
+        ],
+        "iconbar": {
+            "add": true,
+            "top": [
+                "<a href='#'><i class='fa fa-home'></i></a>",
+                "<a href='#'><i class='fa fa-user'></i></a>"
+            ],
+            "bottom": [
+                "<a href='#'><i class='fa fa-twitter'></i></a>",
+                "<a href='#'><i class='fa fa-facebook'></i></a>",
+                "<a href='#'><i class='fa fa-linkedin'></i></a>"
+            ]
+        },
+        "navbars": [
+            {
+                "position": "bottom",
+                "content": [
+                    "<a class='fa fa-envelope' href='#'></a>",
+                    "<a class='fa fa-twitter' href='#'></a>",
+                    "<a class='fa fa-facebook' href='#'></a>"
+                ]
+            }
+        ],
+        "setSelected": {
+            "hover": true,
+            "parent": true
+        }
+    }, {});
+
+    let $hamburger = $(".hamburger");
+    let api = $("#my-menu").data( "mmenu" );
+
+    api.bind("open:finish", () => {
+        $hamburger.addClass("is-active");
+    }).bind("close:finish", () => {
+        $hamburger.removeClass("is-active");
+    });
+
+
+
 
 });

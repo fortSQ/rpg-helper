f6e9db15e74484cc78c6445a9b928de19081b1b4
Akim S. Gubanov <akim_now@mail.ru>
2018-10-19 20:10:41 +0300

    Working

--- src/Helpers/MoneyDecorator.php
+++ src/Helpers/MoneyDecorator.php
@@ -4,16 +4,23 @@ namespace App\Helpers;
 
 class MoneyDecorator
 {
-    const TYPE_PLATINUM = 'pp';
-    const TYPE_GOLD     = 'gp';
-    const TYPE_SILVER   = 'sp';
-    const TYPE_COPPER   = 'cp';
+    const TYPE_PLATINUM = 'P';
+    const TYPE_GOLD     = 'G';
+    const TYPE_SILVER   = 'S';
+    const TYPE_COPPER   = 'C';
 
     const RATION_IN_COPPER = [
         self::TYPE_PLATINUM => 1000,
         self::TYPE_GOLD     => 100,
         self::TYPE_SILVER   => 10,
         self::TYPE_COPPER   => 1,
+    ];
+
+    const TYPE_TO_STRING = [
+        self::TYPE_PLATINUM => 'pp',
+        self::TYPE_GOLD     => 'gp',
+        self::TYPE_SILVER   => 'sp',
+        self::TYPE_COPPER   => 'cp',
     ];
 
     private $amount = 0;
@@ -47,10 +54,16 @@ class MoneyDecorator
 
             list($count, $amount) = $this->test($amount, $type);
             if ($count or $showEmptyValues) {
-                $string .= ' ' . $count . $type;
+                $string .= ' ' . $count . self::TYPE_TO_STRING[$type];
             }
         }
 
         return ltrim($string);
+    }
+
+    public function __toString()
+    {
+        // @example 1002 => 10gp 2cp
+        return $this->toString();
     }
 }
